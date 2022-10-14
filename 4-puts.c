619a65a804152b3fcf4e0ea975239c39f110a30f
Mustopha Yunus <mustophayunus6@gmail.com>
2022-10-14 04:31:02 -0700

    task3

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main: This is the entry point of any c program
+ * Return: (0)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle,\n");
+	return (0);
+}
