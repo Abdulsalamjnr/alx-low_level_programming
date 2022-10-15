26f2c6f0ed0910534a326710cac93015f4cf21bd
Mustopha Yunus <mustophayunus6@gmail.com>
2022-10-15 10:33:45 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - This is entry point of every C program
+ * main - This is the entry point of every C program
  * Return: (0)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
