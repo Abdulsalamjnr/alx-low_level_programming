13556bb70dea907ed7ee9440ac4c8c2c15a84eb3
Mustopha Yunus <mustophayunus6@gmail.com>
2022-11-14 21:03:58 -0800

    all tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,20 +6,20 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
+ * Description: This struct is for dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_database;
+}; 
 
-void init_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog *);
-dog_t *new_dog(char *, float, char *);
-int _strlen(char *);
-char *_memcpy(char *, char *, unsigned int);
-void free_dog(dog_t *);
+typedef  struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
